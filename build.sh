4e71a6c8b3cbe494a4af1109c778ca51488f8581
Keith Lockwood <kwlockwo@gmail.com>
2023-07-21 10:46:06 +1000

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-#echo "forcing new pnpm"
-#npm install -g pnpm
+echo "pnpm: $(pnpm --version)"
+
+echo "forcing new pnpm"
+npm install -g pnpm
 
 echo "node: $(node --version)"
 echo "pnpm: $(pnpm --version)"
